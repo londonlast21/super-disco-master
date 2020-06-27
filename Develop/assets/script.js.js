fb1be763dfae417ddd9c0267b0d929e2ee4167db
jack <biffisbuffm@gmail.com>
2020-06-27 15:15:04 -0500

    more edits

--- Develop/assets/script.js.js
+++ Develop/assets/script.js.js
@@ -1,1 +1,19 @@
-window.alert("hi");
+//variables section//
+
+//today's date
+var todayDate = moment().format('YYYY-MM-DD');
+//tester 
+console.log(todayDate);
+
+// end var section
+
+$("<p>" + todayDate + "<p>").appendTo("currentDay.lead");
+
+
+//onclick to edit events
+$("col-md-9").click(function(){
+    alert("The paragraph was clicked.");
+  });
+
+
+
