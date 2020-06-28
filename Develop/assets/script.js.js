2cb89070f91df0b94ee860e371eba8ecb66c6f56
jack <biffisbuffm@gmail.com>
2020-06-27 19:36:12 -0500

    all tasks save to localStorage

--- Develop/assets/script.js.js
+++ Develop/assets/script.js.js
@@ -5,13 +5,61 @@ var todayDate = moment().format('YYYY-MM-DD');
 $("#currentDay").text(todayDate);
 
 //onclick event saves textarea content to localStorage
-$(".fa.fa-lock").on("click", function() {
+$(".fa.fa-lock.9a").on("click", function() {
     //get nearby values
-    var event = $.trim($("#events9").val());
+    var event = $.trim($("#events9a").val());
 
-    localStorage.setItem("task9am", JSON.stringify(event));
+    localStorage.setItem("task9a", JSON.stringify(event));
 
-    console.log(localStorage)
+});
 
-})
+$(".fa.fa-lock.10a").on("click", function() {
+    var event = $.trim($("#events10a").val());
+
+    localStorage.setItem("task10a", JSON.stringify(event));
+
+});
+
+$(".fa.fa-lock.11a").on("click", function() {
+    var event = $.trim($("#events11a").val());
+
+    localStorage.setItem("task11a", JSON.stringify(event));
+});
+
+$(".fa.fa-lock.12p").on("click", function() {
+    var event = $.trim($("#events12p").val());
+
+    localStorage.setItem("task12p", JSON.stringify(event));
+});
+
+$(".fa.fa-lock.1p").on("click", function() {
+    var event = $.trim($("#events1p").val());
+
+    localStorage.setItem("task1p", JSON.stringify(event));
+});
+
+$(".fa.fa-lock.2p").on("click", function() {
+    var event = $.trim($("#events2p").val());
+
+    localStorage.setItem("task2p", JSON.stringify(event));
+});
+
+$(".fa.fa-lock.3p").on("click", function() {
+    var event = $.trim($("#events3p").val());
+
+    localStorage.setItem("task3p", JSON.stringify(event));
+});
+
+$(".fa.fa-lock.4p").on("click", function() {
+    var event = $.trim($("#events4p").val());
+
+    localStorage.setItem("task4p", JSON.stringify(event));
+});
+
+$(".fa.fa-lock.5p").on("click", function() {
+    var event = $.trim($("#events5p").val());
+
+    localStorage.setItem("task5p", JSON.stringify(event));
+});
+
 });
